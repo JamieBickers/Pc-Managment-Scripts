7c2fb7a8d1a00848e890c90d7ba9c1ebd83e1e50
JamieBickers <bickersjamie@googlemail.com>
2018-01-09 18:58:49 +0000

    Prevent acting on email already acted upon.

--- StartupOnEmail.py
+++ StartupOnEmail.py
@@ -2,11 +2,11 @@ import imaplib
 import email
 import time
 from datetime import datetime
+from dateutil.parser import parse
 import RPi.GPIO as GPIO
 import time
 
-def startPc():
-    print("starting...")
+def start_pc():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(18, GPIO.OUT)
@@ -14,7 +14,7 @@ def startPc():
     time.sleep(1)
     GPIO.output(18, GPIO.LOW)
     
-for i in range(0, 1):
+def time_of_last_startup_email():
     server= imaplib.IMAP4_SSL("imap.gmail.com")
     server.login("jamiebickerspcmanager@googlemail.com", "yM05YkJWGirq")
     server.select()
@@ -25,7 +25,30 @@ for i in range(0, 1):
     message = email.message_from_string(decoded)
     date = message["Date"].replace(",", " ").replace(":", " ")[:-6] # put it into the format that can be read below
     date_object = datetime.strptime(date, "%a %d %b %Y %H %M %S")
-    difference = datetime.now()-date_object
-    if difference.total_seconds() < 180:
-        startPc()
-    time.sleep(1)
+    return date_object
+
+def read_last_startup_time_from_file():
+    with open("/home/pi/Desktop/StartupHandledTimes.txt") as file:
+        date = file.read()
+        return parse(date)
+        
+def write_last_startup_time_to_file(date):
+    with open("/home/pi/Desktop/StartupHandledTimes.txt", "w") as file:
+        file.write(str(date))
+
+def main():
+    try:
+        handled_times = [read_last_startup_time_from_file()]
+    except:
+        handled_times = []
+    for i in range(0, 360):
+        time_of_email = time_of_last_startup_email()
+        difference = datetime.now()-time_of_email
+        if difference.total_seconds() < 180:
+            if time_of_email not in handled_times:
+                start_pc()
+                handled_times = [time_of_email]
+                write_last_startup_time_to_file(time_of_email)
+        time.sleep(10)
+
+main()
